98ee2cfd9ec8b239b8c528388756f04013af17bb
lilian516 <115982783+lilian516@users.noreply.github.com>
2022-11-09 14:53:03 +0100

    Add files via upload

---
+++ pierre_feuille_ciseau.py
@@ -0,0 +1,30 @@
+# on admet une fonction random qui retourne un chiffre aléatoire entre 0 et 2
+# DEBUT
+# On exécute une fonction pierreFeuilleCiseaux    
+    # on execute la fonction input pour demander au joueur si il choisit pierre feuille ciseaux et on stocke ce résultat dans la variable choix
+    # si choix est égale à pierre
+        # alors si random est égale à 0
+            # afficher : l'ordinateur à aussi fait pierre, vous êtes à égalité
+        # alors sinon si random est égale à 1
+            # afficher : l'ordinateur à fait feuille, vous avez perdu
+        # alors sinon
+            # afficher : l'ordinateur à fait ciseaux, vous avez gagnez
+    # sinon si choix est égale à feuille
+        # alors si random est égale à 0
+            # afficher : l'ordinateur à fait pierre, vous avez gagnez
+        # alors sinon si random est égale à 1
+            # afficher : l'ordinateur à fait feuille, vous êtes à égalité
+        # alors sinon
+            # afficher : l'ordinateur à fait ciseaux, vous avez perdu
+    # sinon
+        # alors si random est égale à 0
+            # afficher : l'ordinateur à fait pierre, vous avez perdu
+        # alors sinon si random est égale à 1
+            # afficher : l'ordinateur à fait feuille, vous avez gagnez
+        # alors sinon
+            # afficher : l'ordinateur à fait ciseaux, vous êtes à égalité
+#FIN
+
+
+
+         
